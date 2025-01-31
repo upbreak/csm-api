ceffe493ddbe5b0868205e1c39664ba05482c797
kimjinwoo <upbreak05@naver.com>
2025-01-31 16:38:11 +0900

    dev, prod 환경변수 구분
    jenkins에 8080포트를 사용하게 되면서 로컬에서는 8081포트를 쓰기 위하여 build tags를 이용하여 개발과 환경을 구분 시켰다
    
    *** 명령어
    go run -tags dev/prod .
    go build -tags dev/prod -o app .

---
+++ csm-api/config/config_dev.go
@@ -0,0 +1,21 @@
+//go:build dev
+
+package config
+
+import (
+	"log"
+	"os"
+)
+
+func init() {
+	// 개발 환경에 필요한 기본값 설정
+	err := os.Setenv("ENV", "development")
+	if err != nil {
+		log.Fatalf("Failed to set ENV: %v", err)
+	}
+
+	err = os.Setenv("PORT", "8081")
+	if err != nil {
+		log.Fatalf("Failed to set PORT: %v", err)
+	}
+}
