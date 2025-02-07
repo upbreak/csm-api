be001041e8b0378f8d76c10e0cc2b7dd0351acb4
kimjinwoo <upbreak05@naver.com>
2025-02-07 09:51:14 +0900

    코드 관리 리팩토링
    현장관리의 현장목록 코드에 색상 컬럼을 추가하여 db에서 관리할 수 있도록 변경을 했고 쿼리 변경

--- csm-api/entity/code.go
+++ csm-api/entity/code.go
@@ -3,17 +3,19 @@ package entity
 import "database/sql"
 
 type Code struct {
-	Code   string `json:"code"`
-	PCode  string `json:"p_code"`
-	CodeNm string `json:"code_nm"`
+	Code      string `json:"code"`
+	PCode     string `json:"p_code"`
+	CodeNm    string `json:"code_nm"`
+	CodeColor string `json:"code_color"`
 }
 
 type Codes []*Code
 
 type CodeSql struct {
-	Code   sql.NullString `db:"CODE"`
-	PCode  sql.NullString `db:"P_CODE"`
-	CodeNm sql.NullString `db:"CODE_NM"`
+	Code      sql.NullString `db:"CODE"`
+	PCode     sql.NullString `db:"P_CODE"`
+	CodeNm    sql.NullString `db:"CODE_NM"`
+	CodeColor sql.NullString `db:"CODE_COLOR"`
 }
 
 type CodeSqls []*CodeSql
@@ -22,6 +24,7 @@ func (c *Code) ToCode(sql *CodeSql) *Code {
 	c.Code = sql.Code.String
 	c.PCode = sql.PCode.String
 	c.CodeNm = sql.CodeNm.String
+	c.CodeColor = sql.CodeColor.String
 
 	return c
 }
--- csm-api/store/store_code.go
+++ csm-api/store/store_code.go
@@ -9,12 +9,16 @@ import (
 func (r *Repository) GetCodeList(ctx context.Context, db Queryer, pCode string) (*entity.CodeSqls, error) {
 	sqls := entity.CodeSqls{}
 
-	query := `SELECT
-				t1.CODE,
-				t1.P_CODE,
-				t1.CODE_NM 
-			FROM IRIS_CODE_SET t1
-			WHERE P_CODE = :1`
+	query := `
+				SELECT
+					   t1.CODE,
+				 	   t1.P_CODE,
+					   t1.CODE_NM,
+					   t1.CODE_COLOR
+			      FROM IRIS_CODE_SET t1
+			     WHERE P_CODE = :1
+			       AND t1.IS_USE = 'Y'
+			  ORDER BY t1."ORDER"`
 
 	if err := db.SelectContext(ctx, &sqls, query, pCode); err != nil {
 		return nil, fmt.Errorf("GetCodeList err: %w", err)
