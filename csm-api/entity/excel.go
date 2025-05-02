4af7ebb7b647476395d4c3a9c0456d29915d8f4e
kimjinwoo <upbreak05@naver.com>
2025-05-02 09:36:10 +0900

    퇴직공제 엑셀 내려받기 임시 작성
    github.com/xuri/excelize/v2 라이브러리를 이용하여 퇴직공제 데이터를 엑셀로 내려받는 기능을 임시로 작성

---
+++ csm-api/entity/excel.go
@@ -0,0 +1,25 @@
+package entity
+
+type DailyDeduction struct {
+	Value1  string `json:"value1"`
+	Value2  string `json:"value2"`
+	Value3  string `json:"value3"`
+	Value4  string `json:"value4"`
+	Value5  string `json:"value5"`
+	Value6  string `json:"value6"`
+	Value7  string `json:"value7"`
+	Value8  string `json:"value8"`
+	Value9  string `json:"value9"`
+	Value10 string `json:"value10"`
+	Value11 string `json:"value11"`
+	Value12 string `json:"value12"`
+	Value13 string `json:"value13"`
+	Value14 string `json:"value14"`
+	Value15 string `json:"value15"`
+	Value16 string `json:"value16"`
+	Value17 string `json:"value17"`
+	Value18 int    `json:"value18"`
+	Value19 int    `json:"value19"`
+	Value20 string `json:"value20"`
+	Value21 int    `json:"value21"`
+}
