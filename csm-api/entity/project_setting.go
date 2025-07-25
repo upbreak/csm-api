1a7f63273d5c12b9e56a575cf38b777f98fdd0fc
정지영(99) <jyjeong2412@htenc.co.kr>
2025-07-25 17:07:41 +0900

    [프로젝트 설정] 프로젝트 설정 정보 불러올 경우에 취소코드와 일치하는 마감취소 기간 보내기.

--- csm-api/entity/project_setting.go
+++ csm-api/entity/project_setting.go
@@ -4,10 +4,11 @@ import "github.com/guregu/null"
 
 type ProjectSetting struct {
 	Jno           null.Int    `json:"jno" db:"JNO"`
-	InTime        null.Time   `json:"in_time" db:"IN_TIME"`               // 출근시간
-	OutTime       null.Time   `json:"out_time" db:"OUT_TIME"`             // 퇴근시간
-	RespiteTime   null.Int    `json:"respite_time" db:"RESPITE_TIME"`     // 출/퇴근 유예시간(분)
-	CancelCode    null.String `json:"cancel_code" db:"CANCEL_CODE"`       // 마감취소가능기한 CODE
+	InTime        null.Time   `json:"in_time" db:"IN_TIME"`           // 출근시간
+	OutTime       null.Time   `json:"out_time" db:"OUT_TIME"`         // 퇴근시간
+	RespiteTime   null.Int    `json:"respite_time" db:"RESPITE_TIME"` // 출/퇴근 유예시간(분)
+	CancelCode    null.String `json:"cancel_code" db:"CANCEL_CODE"`   // 마감취소가능기한 CODE
+	CancelDay     null.Int    `json:"cancel_day" db:"CANCEL_DAY"`
 	ManHours      *ManHours   `json:"man_hours"`                          // 공수 정보
 	Message       null.String `json:"message" db:"MESSAGE"`               // 로그 message
 	ChangeSetting null.String `json:"change_setting" db:"CHANGE_SETTING"` // 변경된 테이블
